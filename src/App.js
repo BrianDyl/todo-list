bf0908017e9644278fa8ee1056e5d3910943ddea
BrianDyl <brianadyl@aol.com>
2020-05-05 14:57:43 -0400

    imported fort awesom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
